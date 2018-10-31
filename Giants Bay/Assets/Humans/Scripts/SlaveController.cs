8cdfc2b7c6e5006785f8cba51acf31a9f41148ca
Josh Burt <jburt@bosotability.com>
2018-10-31 16:57:35 -0600

    Added human Class and Human Controllers

---
+++ Giants Bay/Assets/Humans/Scripts/SlaveController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlaveController : HumanClass {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
