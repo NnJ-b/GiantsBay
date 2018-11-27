185d7a91c800e6968139dc97171f2d3be44fc0c4
Josh Burt <jburt@bosotability.com>
2018-11-26 17:02:33 -0700

    Added Temp Camera Elevation

--- Giants Bay/Assets/Player/PlayerCamera/Scripts/CameraController.cs
+++ Giants Bay/Assets/Player/PlayerCamera/Scripts/CameraController.cs
@@ -14,6 +14,9 @@ public class CameraController : MonoBehaviour {
     public float minZoom;
     public float zoomSpeed;
     public Vector3 offset;
+    public float elevation;
+    public float maxElevation;
+    public float minElevation;
     [Range(0.0001f, 1)]
     public float camSmooth = 1f;
 
@@ -42,13 +45,16 @@ public class CameraController : MonoBehaviour {
         if(allowCamRotation)
         {
             float rotation = Input.GetAxis("Horizontal") * rotationSpeed * -1;
-            CamRotation(rotation); 
+            CamRotation(rotation);
+            float elevation = Input.GetAxis("Vertical") * rotationSpeed;
+            CamElevation(elevation);
+            ClampElevation();
         }     
         
         
 
         //maintain position with player
-        Vector3 newPos = player.transform.position + offset * zoom;
+        Vector3 newPos = player.transform.position + new Vector3(offset.x,offset.y + elevation, offset.z) * zoom;
         transform.position = Vector3.Slerp(transform.position, newPos, camSmooth);
         transform.LookAt(player.transform);
     }   
@@ -66,10 +72,26 @@ public class CameraController : MonoBehaviour {
             zoom = minZoom;
         }
     }
+    void ClampElevation()
+    {
+        if (elevation > maxElevation)
+        {
+            elevation = maxElevation;
+        }
+        else if (elevation < minElevation)
+        {
+            elevation = minElevation;
+        }
+    }
 
     private void CamRotation(float rotation)
     {
         Quaternion camTurnAngle = Quaternion.AngleAxis(rotation, Vector3.up);
         offset = camTurnAngle * offset;              
-    }    
+    }
+
+    private void CamElevation(float rotation)
+    {
+        elevation += rotation;
+    }
 }
