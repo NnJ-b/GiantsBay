5a0fb91c8bcb0a317d97273494c2e5b6640083cd
jbur4556 <jbur4556@gmail.com>
2019-01-06 19:03:48 -0700

    Add'd giant mesh and animations and Locomotion

---
+++ Giants Bay0.2/Assets/Characters/Enemy/Giant/Scripts/GiantAnimationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantAnimationController : MonoBehaviour
+{
+    [Header("References")]
+    public GiantMotor motor;
+    public Animator animator;
+    [Header("Animation Controlls")]
+    public float animationSmoothTime;
+
+
+    
+
+    void Update()
+    {
+        float speedPercent = motor.navMeshAgent.velocity.magnitude / motor.navMeshAgent.speed;
+        animator.SetFloat("speedPercent", speedPercent, animationSmoothTime, Time.deltaTime);
+    }
+}
