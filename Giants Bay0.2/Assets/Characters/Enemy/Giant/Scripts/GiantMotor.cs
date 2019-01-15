11f1cf60e1e39d8340a4ba6e160444c21cbe6308
Josh Burt <jburt@bosotability.com>
2019-01-15 16:58:12 -0700

    Giant Pivot manual calculations

--- Giants Bay0.2/Assets/Characters/Enemy/Giant/Scripts/GiantMotor.cs
+++ Giants Bay0.2/Assets/Characters/Enemy/Giant/Scripts/GiantMotor.cs
@@ -11,7 +11,10 @@ public class GiantMotor : MonoBehaviour
 
     [Header("Navigation")]
     public float sightRange = 10f;
-    
+    [Range(0, 10)]
+    public float rotationSpeed = .1f;
+
+
 
     PlayerController player;
 
@@ -19,12 +22,36 @@ public class GiantMotor : MonoBehaviour
     {
         player = PlayerController.instance;
         stateController.onStateChangeCallBack += MoveToPlayer;
-    }    
+    }
+
+    private void Update()
+    {
+        float distanceSquared = (Mathf.Pow(transform.position.x - player.transform.position.x, 2) + Mathf.Pow(transform.position.y - player.transform.position.y, 2) + Mathf.Pow(transform.position.z - player.transform.position.z, 2));
+        if(distanceSquared < Mathf.Pow(navMeshAgent.stoppingDistance,2))
+        {
+            navMeshAgent.updateRotation = false;
+            RotationControl();
+        }
+        else
+        {
+            navMeshAgent.updateRotation = true;
+        }
+    }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.black;
         Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    void RotationControl()
+    {
+        Vector3 direction = (player.transform.position - transform.position);
+        Vector3 x = new Vector3(direction.x, 0f, direction.z);
+        Quaternion lookRotation = Quaternion.LookRotation(x);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+        Debug.Log(x);
+
     }
 
     void MoveToPlayer(EnemyStates state)
