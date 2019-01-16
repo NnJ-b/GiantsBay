17c1a47fafa2454941b250fa8d7b10bf1cc85a1b
Josh Burt <jburt@bosotability.com>
2019-01-16 16:33:27 -0700

    Animation crap

---
+++ Giants Bay0.2/Assets/Characters/Player/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+
+    #region singleton
+    public static PlayerAnimationController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+    public PlayerMotor playerMotor;
+    public Rigidbody rb;
+    public Animator animator;
+
+    public float angleDiff;
+
+    private float maxMagnitude;
+
+    public bool rotating;
+
+    private void Start()
+    {
+        maxMagnitude = (new Vector3(1, 0, 1) * playerMotor.speed).magnitude;
+    }
+
+    private void Update()
+    {
+        CalculateSpeed();
+    }
+
+    public void CalculateSpeed()
+    {
+        float speedPercent = playerMotor.movement.magnitude / maxMagnitude;
+        animator.SetFloat("Speed", speedPercent);
+    }
+
+    public void CalculateRotation(Quaternion newLookRotation)
+    {        
+        if(!rotating && !playerMotor.moving)
+        {
+            Vector3 forwardA = playerMotor.graphics.transform.localRotation * Vector3.forward;
+            Vector3 forwardB = newLookRotation * Vector3.forward;
+
+            float angleA = Mathf.Atan2(forwardA.x, forwardA.z) * Mathf.Rad2Deg;
+            float angleB = Mathf.Atan2(forwardB.x, forwardB.z) * Mathf.Rad2Deg;
+
+            angleDiff = Mathf.DeltaAngle(angleA, angleB);
+
+            animator.SetFloat("Rotation", angleDiff);
+            Debug.Log(angleDiff);
+        }
+    }
+}
--- Giants Bay0.2/Assets/Characters/Player/Scripts/PlayerMotor.cs
+++ Giants Bay0.2/Assets/Characters/Player/Scripts/PlayerMotor.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
+
+    #region singleton
+    public static PlayerMotor instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
     [Header("References")]
     private Rigidbody rb;
     public PlayerController playerController;
     public GameObject graphics;
+    public PlayerAnimationController animationController;
 
-    [Range(1,10)]
+    [Range(.1f,1f)]
     public float speed = 1f;
     [Range(.1f, 1)]
     public float rotationSpeed;
 
+    [HideInInspector]
+    public Vector3 movement;
+    [HideInInspector]
+    public Quaternion childSavedRotation;
+    public bool moving = false;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        childSavedRotation = graphics.transform.rotation;
     }
 
     private void FixedUpdate()
@@ -24,16 +41,36 @@ public class PlayerMotor : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         float xRotation = Input.GetAxis("Mouse X");
-        Vector3 movement = new Vector3(x, 0, y) * speed;
+        movement = new Vector3(x, 0, y) * speed;
 
-        transform.Rotate(Vector3.up, xRotation);
-        transform.Translate(movement);
-        //rb.MovePosition(transform.localPosition + movement);
 
+
+
+        //moving controlls
         if (x != 0f || y != 0f)
         {
-            graphics.transform.localRotation = Quaternion.LookRotation(new Vector3(x, 0, y), Vector3.up);
-            //graphics.transform.localRotation = Quaternion.Slerp(graphics.transform.rotation, Quaternion.LookRotation(new Vector3(x,0,y), Vector3.up), rotationSpeed);
-        }        
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(x, 0, y), Vector3.up);
+            transform.Rotate(Vector3.up, xRotation);
+            
+            //calculates rotation delta
+            animationController.CalculateRotation(lookRotation);
+
+            moving = true;
+            graphics.transform.localRotation = lookRotation;
+            //graphics.transform.localRotation = Quaternion.Slerp(graphics.transform.rotation, lookRotation, rotationSpeed);
+
+            transform.Translate(movement);
+            animationController.CalculateSpeed();
+        }
+
+        //stationary Controlls
+        else
+        {
+            childSavedRotation = graphics.transform.rotation;
+            transform.Rotate(Vector3.up, xRotation);
+            graphics.transform.rotation = childSavedRotation;
+            moving = false;
+            
+        }
     }
 }
