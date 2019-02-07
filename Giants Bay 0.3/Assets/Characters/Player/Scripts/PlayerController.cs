e3a221141a5597a234329e091de5ba9dfe2e6f8a
Josh Burt <jburt@bosotability.com>
2019-02-07 14:39:04 -0700

    a bunch of shit

---
+++ Giants Bay 0.3/Assets/Characters/Player/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //references
+    private PlayerMotor motor = new PlayerMotor();
+    public Rigidbody rb;
+    public GameObject parent;
+    public Animator animator;
+    //movement
+    public float speed = 3f;
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Velocity", rb.velocity.magnitude);
+        MovementControlls();
+        RotationControlls();
+        //animation
+    }
+
+    private void MovementControlls()
+    {
+        //movement    replace input.getaxis when calculating with joysticks
+        rb.velocity = parent.transform.TransformDirection(motor.calculateVelocity(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), speed, rb));
+        //rb.MovePosition(parent.transform.position + motor.calculateMovement(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), speed) * Time.deltaTime);
+    }
+
+    private void RotationControlls()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //stationary controlls
+            if (rb.velocity.magnitude == 0)
+            {
+                Vector3 rotation = transform.eulerAngles;
+                float rotationAmount = Input.GetAxis("Mouse X");
+                parent.transform.localEulerAngles = new Vector3(0, motor.calculateRotation(parent.transform.eulerAngles.y, rotationAmount), 0);
+                transform.eulerAngles = rotation;
+            }
+
+            //moving controlls
+            else
+            {
+                float rotationAmount = Input.GetAxis("Mouse X");
+                parent.transform.localEulerAngles = new Vector3(0, motor.calculateRotation(parent.transform.eulerAngles.y, rotationAmount), 0);
+            }            
+        }
+        //sets graphics rotation based on velocity
+        transform.localRotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
+    }
+}
---
+++ Giants Bay 0.3/Assets/Characters/Player/Scripts/PlayerMotor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor
+{
+    public Vector3 calculateVelocity(float horizontal, float vertical, float speed, Rigidbody rb)
+    {
+
+        Vector3 velocity = new Vector3(horizontal * speed, rb.velocity.y, vertical * speed);
+
+        return velocity;
+    }
+
+    public float calculateRotation(float rotationAmount,float currentRotation)
+    {
+        float newRotation = currentRotation + rotationAmount;
+        return newRotation;
+    }
+}
