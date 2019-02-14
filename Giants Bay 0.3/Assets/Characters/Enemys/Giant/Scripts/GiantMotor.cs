8f9ff78682da4c8806ab13122418d5632e5a7984
Josh Burt <jburt@bosotability.com>
2019-02-14 16:48:44 -0700

    Started Giant

---
+++ Giants Bay 0.3/Assets/Characters/Enemys/Giant/Scripts/GiantMotor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GiantMotor : MonoBehaviour
+{
+    [Header("References")]
+    public NavMeshAgent navMeshAgent;
+    public Animator animator;
+    [Header("AI Controlls")]
+    public float navUpdateTime;
+
+
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(MoveToPlayer());
+    }
+
+    private IEnumerator MoveToPlayer()
+    {
+        navMeshAgent.SetDestination(player.position);
+        yield return new WaitForSeconds(navUpdateTime);
+    }
+
+    private void AnimationState()
+    {
+        animator.SetFloat("Speed", navMeshAgent.velocity.magnitude / navMeshAgent.speed);
+    }
+
+}
